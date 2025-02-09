57282e1ddc0d63ebb3c2e44702689be14c755518
DPRAHUL <145242310+DPRAHUL-2021@users.noreply.github.com>
2025-02-09 10:44:56 +0530

    Update footerLanding.tsx

--- src/Components/footerLanding.tsx
+++ src/Components/footerLanding.tsx
@@ -6,7 +6,7 @@ export default function FooterLanding() {
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
                     <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                        <img className="w-auto h-9" src="./public/Edu_Sphere_Logo__.svg" alt="Logo" />
+                        <img className="w-auto h-9" src="/Edu_Sphere_Logo__.svg" alt="Logo" />
 
                         <p className="text-base leading-relaxed text-white mt-7">
                             Empowering your learning with a community of like-minded individuals. Join us and grow together!
